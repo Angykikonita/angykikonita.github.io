07c441074c781dbd9881c9a2a690f08569d49e2b
Angelica Denisse Garcia Sanchez <109816733+Angykikonita@users.noreply.github.com>
2024-07-09 20:56:22 -0600

    inicial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+document.getElementById('miBoton').addEventListener('click', function() {
+    // Aquí se define la URL a la que quieres redirigir
+    var url = 'https://docs.google.com/forms/d/e/1FAIpQLSdtiGLnjs1MMIgpb9iBps3v1t55b811W431dxN8_8GM60PtZQ/viewform?usp=sf_link';
+    // Redireccionar a la URL
+    window.location.href = url;
+});
